b8b4f1fe8bb3892d30cb0f8ce272d336f0476af2
paulusminus <info@paulmin.nl>
2024-05-19 12:40:32 +0200

    Cargo update

--- connection/src/lib.rs
+++ connection/src/lib.rs
@@ -1,14 +1,14 @@
 #[cfg(target_family = "wasm")]
-#[cfg_attr(target_family = "wasm", path = "spin_sqlite_connection.rs")]
+#[cfg_attr(target_family = "wasm", path = "spin_sqlite.rs")]
 mod connection;
 
 #[cfg(target_family = "wasm")]
-pub use connection::{DbConnection};
+pub use connection::DbConnection;
 
 #[cfg(not(target_family = "wasm"))]
-#[cfg_attr(not(target_family = "wasm"), path = "rusqlite_connection.rs")]
+#[cfg_attr(not(target_family = "wasm"), path = "rusqlite.rs")]
 mod connection;
 
 #[cfg(not(target_family = "wasm"))]
-pub use connection::{DbConnection};
+pub use connection::DbConnection;
 
--- init_data.sh
+++ init_data.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 
-# time curl -X POST https://lipl-storage-spin-gfqbmw44.fermyon.app/lyric -H "Content-Type: application/json" -d @data/roodkapje.json
-# time curl -X POST https://lipl-storage-spin-gfqbmw44.fermyon.app/lyric -H "Content-Type: application/json" -d @data/grachten.json
 time curl -X POST https://lipl.paulmin.nl/db -u paul -H "Content-Type: application/json" -d @data/db.json
