5807d53dd57a9a1a710b705317c75b542ba5b58c
paulusminus <info@paulmin.nl>
2024-05-22 20:22:02 +0200

    rm config.toml

--- download-pwa.fish
+++ download-pwa.fish
@@ -2,5 +2,7 @@
 
 wget https://github.com/paulusminus/lipl-control/releases/latest/download/lipl-pwa.tar.gz
 
+mkdir -p assets
 cd assets
 tar -xzf ../lipl-pwa.tar.gz --no-same-owner --no-same-permissions
+rm ../lipl-pwa.tar.gz
